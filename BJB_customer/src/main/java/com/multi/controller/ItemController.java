13b4cc7bd9bd3bf31806981bae0faaa15b890a56
Yunjinho <dbswlsgh1238@naver.com>
2022-10-11 17:39:31 +0900

    main , item page

--- BJB_customer/src/main/java/com/multi/controller/ItemController.java
+++ BJB_customer/src/main/java/com/multi/controller/ItemController.java
@@ -1,8 +1,13 @@
 package com.multi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ItemController {
-	
+	String dir ="item";
+	@RequestMapping("/item")
+	public String main() {
+		return dir+"/item";
+	}
 }
