23f4f860189afd60ff649db00ae1450be3434c78
kmjeon <kmjeon@DESKTOP-3IN56CK>
2022-10-11 09:43:05 +0900

    first

---
+++ BJB_customer/src/main/java/com/multi/BjbCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjbCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjbCustomerApplication.class, args);
+	}
+
+}
