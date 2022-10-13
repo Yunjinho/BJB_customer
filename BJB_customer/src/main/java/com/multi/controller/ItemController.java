4ddb087d09f475572d399a5db89441c31a919e67
Shindongjoon7 <ehdwns8467@gmail.com>
2022-10-13 14:33:57 +0900

    20221013 아에템 페이지 각 상품들 뿌려주기

--- BJB_customer/src/main/java/com/multi/controller/ItemController.java
+++ BJB_customer/src/main/java/com/multi/controller/ItemController.java
@@ -1,17 +1,40 @@
 package com.multi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.multi.dto.ItemDTO;
+import com.multi.service.ItemService;
+
 @Controller
 public class ItemController {
 	
+	@Autowired
+	ItemService service;
+	
 	String dir = "item/";
 	
-	@RequestMapping("/outer")
+/*	@RequestMapping("/outer")
 	public String outer(Model model) {
 		model.addAttribute("center", dir+"outer");
+		return "index";
+	}*/
+	
+	@RequestMapping("/outer")
+	public String jacket(Model model) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.selectItemAll(11);
+			model.addAttribute("obj", list);
+			model.addAttribute("center", dir+"outer");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		return "index";
 	}
 	
