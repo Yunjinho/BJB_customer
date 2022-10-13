8eecb1cfbfadcc7ba2ea86558d7149cdbf930c3d
hannah <wh467925@gamil.com>
2022-10-13 09:11:01 +0900

    hannah

--- BJB_customer/src/main/java/com/multi/controller/MainController.java
+++ BJB_customer/src/main/java/com/multi/controller/MainController.java
@@ -1,12 +1,24 @@
 package com.multi.controller;
 
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.multi.dto.CustDTO;
+import com.multi.service.CustService;
+
 
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	CustService cust_service;
 	@RequestMapping("/")
 	public String main() {
 		return "index";
@@ -15,6 +27,7 @@ public class MainController {
 	public String clo() {
 		return "categories";
 	}
+
 	@RequestMapping("/contact")
 	public String contact() {
 		return "contact";
