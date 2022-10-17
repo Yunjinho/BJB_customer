bd26b50f65972c299b64f9afd5068c3ea02cd045
parkpirate <skbsy153@gmail.com>
2022-10-17 10:06:21 +0900

    아이템 디테일 카트담기 성공 → 동준이가 카트 색상담기 도전예정

--- BJB_customer/src/main/java/com/multi/controller/AJAXController.java
+++ BJB_customer/src/main/java/com/multi/controller/AJAXController.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.multi.dto.CartDTO;
 import com.multi.mapper.AJAXMapper;
+import com.multi.service.CartService;
 
 @RestController
 public class AJAXController {
+	
 	@Autowired
 	AJAXMapper mapper;
+	
 	@RequestMapping("/likedcnt")
 	public String likedcnt() {
 		int cnt=0;
